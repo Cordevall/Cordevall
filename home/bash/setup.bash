fed7e9deefd201a978db8d51d7ba0243da1780b7
eveeifyeve <eveeg1971@gmail.com>
2023-08-22 14:16:24 +1000

    moved everything into home.

--- home/bash/setup.bash
+++ home/bash/setup.bash
@@ -18,14 +18,21 @@ read DISCORD_TOKEN
 # Save Discord token to .env file
 echo "DISCORD_TOKEN=$DISCORD_TOKEN" >> $ENV_FILE
 
+
+echo "Please enter your client id:"
+read CLIENT_TOKEN
+
+# Save Discord token to .env file
+echo "CLIENT_TOKEN=$CLIENT_TOKEN" >> $ENV_FILE
+
 CODING_LANGUAGE=""
 
 # Keep asking for coding language until a valid one is entered
-while [[ ! "$CODING_LANGUAGE" =~ ^(rust|java|js|python|go|lua|ruby)$ ]]; do
-    echo "Please enter your preferred coding language (rust, java, js, python, go, lua, ruby):"
+while [[ ! "$CODING_LANGUAGE" =~ ^(rust|java|js|python|go|lua|ruby|kotlin|ts)$ ]]; do
+    echo "Please enter your preferred coding language (rust, java, js, python, go, lua, ruby, kotlin, ts):"
     read CODING_LANGUAGE
     # If the input is not one of the allowed languages, print an error message
-    if [[ ! "$CODING_LANGUAGE" =~ ^(rust|java|js|python|go|lua|ruby)$ ]]; then
+    if [[ ! "$CODING_LANGUAGE" =~ ^(rust|java|js|python|go|lua|ruby|kotlin|ts)$ ]]; then
         echo "Invalid language. Please enter a valid language."
     fi
 done
